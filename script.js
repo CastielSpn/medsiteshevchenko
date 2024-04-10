0055b13cb9e6d1d7487bf35a5848bcb71b6cf256
Casifer <57217461+CastielSpn@users.noreply.github.com>
2024-04-11 01:37:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// script.js
+const productCards = document.querySelectorAll('.product-card');
+const onas = document.querySelectorAll('.contact');
+
+productCards.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+    });
+
+    card.addEventListener('mouseout', () => {
+        card.style.boxShadow = 'none';
+    });
+});
+
+onas.forEach(card => {
+    card.addEventListener('mouseover', () => {
+        card.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.5)';
+    });
+
+    card.addEventListener('mouseout', () => {
+        card.style.boxShadow = 'none';
+    });
+});
